2b144dba83a5327ec630e6a1776e53a8ef8be945
Joe Previte <jjprevite@gmail.com>
2019-10-29 08:02:55 -0700

    fix: clean up g-react-component

--- my-package/src/scripts/g-react-component.js
+++ my-package/src/scripts/g-react-component.js
@@ -1,27 +1,21 @@
 // This is the script we run to generate a new React component
-const { execSync, exec } = require('child_process');
+const { execSync } = require('child_process');
 const path = require('path')
-const hygen = require('hygen')
-// 1. Get it to use the template
-// HYGEN_TMPLS=my-package/_templates hygen component new --name
 
 // The args passed when running this script
 const args = process.argv
+
 // The name of the component is the only valid arg at this time.
 // It will come as the last arg.
 const nameOfComponent = args[process.argv.length - 1]
-// We're going to have our users run the script from the place with which they
-// want their component generated.
+
+// Grab the path of the user's project
 const pathWhereScriptIsRunning = process.cwd()
 
 // This is where their templates are (at least we assume so)
 const theirTemplatePath = pathWhereScriptIsRunning + '/_templates'
 
-// console.log('hello from g-react-component', nameOfComponent)
-// console.log('you ran this script from this root: ', pathWhereScriptIsRunning)
-
-// The first thing I want to do is run a command, using our script, and their templates
+// The root of our package, so that we can leverage the scripts in the `package.json`
 const rootDirectory = path.join(__dirname, '../../');
-// execSync(`HYGEN_TEMPLS=${theirTemplatePath} yarn g:component`, { cwd: rootDirectory})
-execSync('yarn test', { cwd: rootDirectory, stdio: 'inherit'})
+
 execSync(`HYGEN_TMPLS=${theirTemplatePath} yarn g:component ${nameOfComponent} --path=${pathWhereScriptIsRunning}`, { cwd: rootDirectory, stdio: 'inherit'})
